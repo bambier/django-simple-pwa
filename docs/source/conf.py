cf5d041a05beb6eb164040af1af730f80e6eae1d
nimaes80 <>
2022-10-02 18:20:46 +0330

    localization

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,11 +41,8 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-# language = 'fa'
-languages = [
-	'fa',
-	"en_US",
-]
+language = 'fa'
+locale_dir = 'locales'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
