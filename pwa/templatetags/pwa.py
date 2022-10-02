6de716ca77e39e69c371bd2b15ff4de90b706c88
nimaes80 <>
2022-10-02 17:22:44 +0330

    fix load Data from dict data

--- pwa/templatetags/pwa.py
+++ pwa/templatetags/pwa.py
@@ -8,13 +8,8 @@ register = template.Library()
 @register.inclusion_tag('pwa/metadata.html')
 def pwa_meta_data():
 	PWA_CONFIG = get_pwa_config()
-	try:
-		ICONS = PWA_CONFIG.get('icons')
-		THEME_COLOR = PWA_CONFIG.get('theme_color')
-	except:
-		ICONS = None
-		THEME_COLOR = None
-
+	ICONS = PWA_CONFIG.get('icons', None)
+	THEME_COLOR = PWA_CONFIG.get('theme_color', None)
 	return {'ICONS':ICONS, 'THEME_COLOR':THEME_COLOR}
 
 
