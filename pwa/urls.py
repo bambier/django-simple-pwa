cab47969296c722e77321802d3d6d9a7b49b1be6
Bambier <nimpel2@proton.me>
2023-06-07 22:37:52 +0330

    Linting

--- pwa/urls.py
+++ pwa/urls.py
@@ -4,12 +4,10 @@ from pwa.views import manifest_json, sw_js, app_js, offline
 app_name = 'pwa'
 
 urlpatterns = [
-	path('sw.js' , sw_js, name="sw.js"),
-	path('manifest.json' , manifest_json, name="manifest.json"),
-	path('app.js', app_js, name="app.js"),
-	
-	path('offline' , offline, name="offline"),
-	
-	
-]
+    path('sw.js', sw_js, name="sw.js"),
+    path('manifest.json', manifest_json, name="manifest.json"),
+    path('app.js', app_js, name="app.js"),
 
+    path('offline', offline, name="offline"),
+
+]
