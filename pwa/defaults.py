6c87c19b9f1385b50486eeaf988280f25a289ba3
Bambier <nimpel2@proton.me>
2023-06-07 22:37:14 +0330

    Update defaults

--- pwa/defaults.py
+++ pwa/defaults.py
@@ -1,79 +1,79 @@
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
-def get_pwa_config():
-	DEFAULT_CONFIG = {
-		"name": _("Progressive Web Application"),
-		"short_name": _("PWA"),
-		"theme_color": "#7820f5",
-		"background_color": "#7820f5",
-		"display": "standalone",
-		"orientation": "portrait",
-		"scope": "/",
-		"start_url": "/",
-		"icons": [
-			{
-				"src": "/static/pwa/images/icons/72x72.png",
-				"type": "image/png",
-				"sizes": "72x72"
-			},
-			{
-				"src": "/static/pwa/images/icons/96x96.png",
-				"type": "image/png",
-				"sizes": "96x96"
-			},
-			{
-				"src": "/static/pwa/images/icons/128x128.png",
-				"type": "image/png",
-				"sizes": "128x128"
-			},
-			{
-				"src": "/static/pwa/images/icons/144x144.png",
-				"type": "image/png",
-				"sizes": "144x144"
-			},
-			{
-				"src": "/static/pwa/images/icons/152x152.png",
-				"type": "image/png",
-				"sizes": "152x152"
-			},
-			{
-				"src": "/static/pwa/images/icons/192x192.png",
-				"type": "image/png",
-				"sizes": "192x192"
-			},
-			{
-				"src": "/static/pwa/images/icons/384x384.png",
-				"type": "image/png",
-				"sizes": "384x384"
-			},
-			{
-				"src": "/static/pwa/images/icons/512x512.png",
-				"type": "image/png",
-				"sizes": "512x512"
-			}
-		],
-		"lang": "en",
-		"dir": "ltr",
-		"description": _("Progressive Web app powered by Django"),
-		"version": "1.",
-		"manifest_version": "1.0",
-		"permissions": [
-			"notifications",
-			"webRequest"
-		],
-		"author": _("PWA-django"),
-		}
-	try:
-		if settings.PWA_CONFIG and settings.PWA_CONFIG != {}:
-			return settings.PWA_CONFIG
-	except:
-		return DEFAULT_CONFIG
 
+def get_pwa_config():
+    DEFAULT_CONFIG = {
+        "name": _("Progressive Web Application"),
+        "short_name": _("PWA"),
+        "theme_color": "#7820f5",
+        "background_color": "#7820f5",
+        "display": "standalone",
+        "orientation": "portrait",
+        "scope": "/",
+        "start_url": "/",
+        "icons": [
+            {
+                "src": "/static/pwa/images/icons/72x72.png",
+                "type": "image/png",
+                            "sizes": "72x72"
+            },
+            {
+                "src": "/static/pwa/images/icons/96x96.png",
+                "type": "image/png",
+                "sizes": "96x96"
+            },
+            {
+                "src": "/static/pwa/images/icons/128x128.png",
+                "type": "image/png",
+                "sizes": "128x128"
+            },
+            {
+                "src": "/static/pwa/images/icons/144x144.png",
+                "type": "image/png",
+                "sizes": "144x144"
+            },
+            {
+                "src": "/static/pwa/images/icons/152x152.png",
+                "type": "image/png",
+                "sizes": "152x152"
+            },
+            {
+                "src": "/static/pwa/images/icons/192x192.png",
+                "type": "image/png",
+                "sizes": "192x192"
+            },
+            {
+                "src": "/static/pwa/images/icons/384x384.png",
+                "type": "image/png",
+                "sizes": "384x384"
+            },
+            {
+                "src": "/static/pwa/images/icons/512x512.png",
+                "type": "image/png",
+                "sizes": "512x512"
+            }
+        ],
+        "lang": "en",
+        "dir": "ltr",
+        "description": _("Progressive Web app powered by Django"),
+        "version": "1.",
+        "manifest_version": "1.0",
+        "permissions": [
+            "notifications",
+            "webRequest"
+        ],
+        "author": _("PWA-django"),
+    }
+    try:
+        if settings.PWA_CONFIG and settings.PWA_CONFIG != {}:
+            return settings.PWA_CONFIG
+    except:
+        return DEFAULT_CONFIG
 
 
 def get_service_worker():
-	SERVICE_WORKER = """
+    SERVICE_WORKER = """
 var CACHE_NAME = 'pwa-cache-v1';
 var urlsToCache = [
 	'/',
@@ -126,18 +126,18 @@ self.addEventListener('activate', (event) =>{
 		)
 });
 	""" % (
-		_("Cache Opend."),
-		
-		)
-	try:
-		if settings.PWA_SW and settings.PWA_SW != {}:
-			return settings.PWA_SW
-	except:
-		return SERVICE_WORKER
+        _("Cache Opend."),
+
+    )
+    try:
+        if settings.PWA_SW and settings.PWA_SW != {}:
+            return settings.PWA_SW
+    except:
+        return SERVICE_WORKER
 
 
 def get_app():
-	APP = """
+    APP = """
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
 		navigator.serviceWorker
@@ -149,13 +149,48 @@ if ("serviceWorker" in navigator) {
 	console.log(`%s`);
 };
 	""" % (
-		_("Service worker registered!"),
-		_("Your browser supports serviceWorker, but the service worker is not registered."),
-		_("Your browser doesn't support serviceWorker, so you can't install the PWA."),
-		)
-	try:
-		if settings.PWA_APP and settings.PWA_APP != {}:
-			return settings.PWA_APP
-	except:
-		return APP
+        _("Service worker registered!"),
+        _("Your browser supports serviceWorker, but the service worker is not registered."),
+        _("Your browser doesn't support serviceWorker, so you can't install the PWA."),
+    )
+    try:
+        if settings.PWA_APP and settings.PWA_APP != {}:
+            return settings.PWA_APP
+    except:
+        return APP
 
+
+
+
+PERMISSION_LIST = """
+activeTab
+background
+bookmarks
+browserSettings
+clipboardRead
+clipboardWrite
+contentSettings
+contextMenus
+cookies
+debugger
+declarativeNetRequestFeedback
+downloads
+downloads.open
+find
+geolocation
+history
+idle
+management
+nativeMessaging
+notifications
+pageCapture
+privacy
+scripting
+tabHide
+tabs
+topSites
+webNavigation
+webRequest
+webRequestBlocking
+webRequestFilterResponse
+webRequestFilterResponse.serviceWorkerScript"""
