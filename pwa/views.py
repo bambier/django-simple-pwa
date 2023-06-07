8c88c34707d4dfb66304a7e4f1cc0a03a0e366b0
Bambier <nimpel2@proton.me>
2023-06-07 22:37:40 +0330

    Linting

--- pwa/views.py
+++ pwa/views.py
@@ -7,19 +7,19 @@ from pwa.defaults import get_app, get_pwa_config, get_service_worker
 
 
 def manifest_json(request):
-	return JsonResponse(get_pwa_config())
+    return JsonResponse(get_pwa_config())
 
 
 def sw_js(request):
-	return HttpResponse(get_service_worker() , content_type='application/javascript')
+    return HttpResponse(get_service_worker(), content_type='application/javascript')
 
 
 def app_js(request):
-	return HttpResponse(get_app() , content_type='application/javascript')
-	
+    return HttpResponse(get_app(), content_type='application/javascript')
+
 
 def offline(request):
-	try:
-		return render(request, 'pwa/offline.html')
-	except:
-		return render(request, 'pwa/pwa_offline.html')
+    try:
+        return render(request, 'pwa/offline.html')
+    except:
+        return render(request, 'pwa/pwa_offline.html')
