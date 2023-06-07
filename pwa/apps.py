1466ac8bd79a514ab3f74bff5575c08b60df1779
Bambier <nimpel2@proton.me>
2023-06-07 22:36:49 +0330

    Add verbose name for admin app

--- pwa/apps.py
+++ pwa/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class PwaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pwa'
+    verbose_name = _("PWA")
